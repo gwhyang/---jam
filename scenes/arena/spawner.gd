a94453779c2649d12873b47060775e3b25b9e96e
gwhyang <139050383+gwhyang@users.noreply.github.com>
2026-04-19 02:44:40 +0800

    random summon logic

--- scenes/arena/spawner.gd
+++ scenes/arena/spawner.gd
@@ -4,6 +4,7 @@ class_name Spawner
 signal on_wave_completed
 
 @export var spawn_area_size := Vector2(1000, 500)
+@export var summon_range:= Vector2(200, 200)
 @export var waves_data: Array[WaveData]
 @export var enemy_collection: Array[UnitStats]
 
@@ -84,7 +85,31 @@ func spawn_enemy() -> void:
 	
 	set_spawn_timer()
 
+func spawn_summon(summon_scene:PackedScene)->void:
+	var summon_posi := get_random_summon_position()
+	var summon_anim := spawn(Global.SUMMON_EFFECT_SCENE,summon_posi)
+	if not summon_anim:
+		push_error(0000)
+		return
+	await summon_anim.anim_player.animation_finished
+	summon_anim.queue_free()
+		
+	var summon_instance := spawn(summon_scene,summon_posi) as Summon
+	spawned_summons.append(summon_instance)
 
+func get_random_summon_position() -> Vector2:
+	if not Global.player:
+		push_error('Try summon when player not initialized.')
+		return Vector2.INF
+	var range:Array[float] = [0,0,0,0]
+	var player_posi = Global.player.global_position
+	range[0] = maxf(-spawn_area_size.x,player_posi.x-summon_range.x)
+	range[1] = minf(spawn_area_size.x,player_posi.x+summon_range.x)
+	range[2] = maxf(-spawn_area_size.y,player_posi.y-summon_range.y)
+	range[3] = minf(spawn_area_size.y,player_posi.y+summon_range.y)
+	return Vector2(randf_range(range[0],range[1]),randf_range(range[2],range[3]))
+	
+	
 func update_enemies_new_wave() -> void:
 	for stat: UnitStats in enemy_collection:
 		stat.health += stat.health_increase_per_wave
