fb0dc6d5816e92888673d475791632b31ccc910e
gwhyang <139050383+gwhyang@users.noreply.github.com>
2026-04-20 21:49:29 +0800

    uid文件

--- fortest/try_spawn_summon.gd
+++ fortest/try_spawn_summon.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
 @export var summon:PackedScene
+@onready var spawner: Spawner = $"../Spawner"
 
 var is_testing:bool = false
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("test"):
 		is_testing = !is_testing
+		spawner.random_spawn_summon(summon)
 	if event.is_action_pressed("atk") and is_testing:
 		if summon:
 			Game.spawn(summon,get_global_mouse_position())
