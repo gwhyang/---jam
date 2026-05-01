7aa93fe54fd7abbae5a6fadf7ff20443500b074b
gwhyang <139050383+gwhyang@users.noreply.github.com>
2026-05-02 01:43:42 +0800

    Selection panel add initial item

--- scenes/ui/selection_panel/selection_panel.gd
+++ scenes/ui/selection_panel/selection_panel.gd
@@ -5,22 +5,29 @@ signal on_selection_completed
 
 @export var players: Array[UnitStats]
 @export var start_weapons: Array[ItemWeapon]
+@export var start_item:Array[BoneItem]##chose initial skull
+@export var equip_panel:EquipPanel
 
 @onready var player_container: HBoxContainer = %PlayerContainer
 @onready var weapon_container: HBoxContainer = %WeaponContainer
+@onready var item_container: HBoxContainer = %ItemContainer
 
 @onready var player_icon: TextureRect = %PlayerIcon
 @onready var player_name: Label = %PlayerName
 @onready var player_title: Label = %PlayerTitle
 @onready var player_description: RichTextLabel = %PlayerDescription
 
+var initial_item_card:ItemCard
+
 func _ready() -> void:
 	for child in player_container.get_children(): child.queue_free()
 	for child in weapon_container.get_children(): child.queue_free()
+	for child in item_container.get_children(): child.queue_free()
 	
 	show_player_info(false)
 	load_players()
 	load_weapons()
+	load_items()
 	
 
 
@@ -45,7 +52,15 @@ func load_weapons() -> void:
 		weapon_container.add_child(card)
 		card.icon = weapon.item_icon
 		
-
+func load_items():
+	if start_item.is_empty():
+		return
+	
+	for item: BoneItem in start_item:
+		var card: SelectionCard = Global.SELECTION_CARD_SCENE.instantiate()
+		card.pressed.connect(_on_item_selected.bind(item))
+		item_container.add_child(card)
+		card.icon = item.item_icon
 
 func show_player_info(value: bool) -> void:
 	player_icon.visible = value
@@ -62,13 +77,24 @@ func _on_player_selected(player: UnitStats) -> void:
 	player_name.text = player.name
 	player_description.text = "[code]Health: [color=green]%s[/color]\nDamage: [color=green]%s[/color]\nSpeed: [color=green]%s[/color]\nLuck: [color=green]%s[/color]\nBlock Chance: [color=green]%s%%[/color][/code]" % [player.health, player.damage, player.speed, player.luck, player.block_chance]
 
+func _on_item_selected(item:BoneItem):
+	if not initial_item_card:
+		initial_item_card = equip_panel.add_equipment_item_card(item)
+	else:
+		initial_item_card.item = item
+	
+	initial_item_card.hide()
+	equip_panel.equip_item(Global.BoneSlot.head,item)
+	equip_panel.slot_card[Global.BoneSlot.head] = initial_item_card
 
 func _on_weapon_selected(weapon: ItemWeapon) -> void:
 	Global.main_weapon_selected = weapon
 	
 
 func _on_continue_button_pressed() -> void:
-	if not Global.main_player_selected and not Global.main_weapon_selected:
+	if not Global.main_player_selected \
+		and not Global.main_weapon_selected \
+		and Global.equiped_bones[Global.BoneSlot.head]:
 		return
 	
 	on_selection_completed.emit()
