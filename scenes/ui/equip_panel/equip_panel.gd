bb7d04d6e4a25c9e89413191b28c67c73d3d05ca
gwhyang <139050383+gwhyang@users.noreply.github.com>
2026-05-01 18:19:19 +0800

    Update equip_panel.gd

--- scenes/ui/equip_panel/equip_panel.gd
+++ scenes/ui/equip_panel/equip_panel.gd
@@ -1,5 +1,8 @@
 extends Panel
+class_name EquipPanel
+
 signal on_equip_panel_next_wave
+signal shop_pressed
 
 var head=Global.BoneSlot.head
 var lhand=Global.BoneSlot.lhand
@@ -34,6 +37,8 @@ var rleg=Global.BoneSlot.rleg
 
 var current_selct_card:ItemCard
 var current_slot:Global.BoneSlot = -1
+var slot_card:Dictionary[Global.BoneSlot,ItemCard]
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -93,10 +98,30 @@ func _on_equip_button_button_down() -> void:
 		return
 	# Move selected inventory item into chosen body slot.
 	var item := current_selct_card.item
-	equip_slots_ui[current_slot].item = item
-	current_selct_card.queue_free()
+	#current_selct_card.queue_free()
+	current_selct_card.hide()
 	
-	Global.equiped_bones[current_slot]= item
+	equip_item(current_slot,item)
+	#equip_slots_ui[current_slot].item = item
+	#Global.equiped_bones[current_slot]= item
+	#item = item as BoneItem
+	#var allowed_triggers:Array[Global.ItemCallBack] = [Global.ItemCallBack.ONEQUIP,Global.ItemCallBack.EQUIPORUNLOAD]
+	#var equip_context:= EffectContext.new()
+	#equip_context.trigger_type = Global.ItemCallBack.ONEQUIP
+	## Trigger all ONEQUIP effects declared on this bone item.
+	#for packed_effect:PackedItemEffect in item.effects:
+		#for trigger in allowed_triggers:
+			#if packed_effect.trigger == trigger:
+				#packed_effect.effect(equip_context)
+	
+	slot_card[current_slot] = current_selct_card
+	current_selct_card = null
+	current_slot = -1
+	equip_button.disabled = true
+
+func equip_item(slot:Global.BoneSlot,item:BoneItem):
+	equip_slots_ui[slot].item = item
+	Global.equiped_bones[slot]= item
 	item = item as BoneItem
 	var allowed_triggers:Array[Global.ItemCallBack] = [Global.ItemCallBack.ONEQUIP,Global.ItemCallBack.EQUIPORUNLOAD]
 	var equip_context:= EffectContext.new()
@@ -106,24 +131,23 @@ func _on_equip_button_button_down() -> void:
 		for trigger in allowed_triggers:
 			if packed_effect.trigger == trigger:
 				packed_effect.effect(equip_context)
-	
-	current_selct_card = null
-	current_slot = -1
-	equip_button.disabled = true
 
-
-func _on_take_off_button_button_down() -> void:
-	if not current_selct_card and current_selct_card.item:
-		push_error("aaaaaa")
+func equip_take_off(slot:Global.BoneSlot):
+	#TODO finish slot card mapping in on_take_off..
+	# TODO finsh this
+	var card:= slot_card[slot]
+	if not card:
+		push_error("No card equiped")
 		return
-	# Move equipped item back to inventory list.
-	var item := current_selct_card.item
-	current_selct_card.item = null
-	add_equipment_item_card(item)
-	
-	
-	Global.equiped_bones[current_slot]= null
-	item = item as BoneItem
+	if not card.item:
+		push_error("Card have no item")
+		return
+	#移出物体
+	slot_card[slot] = null
+	var item = equip_slots_ui[slot].item
+	equip_slots_ui[slot].item = null
+	Global.equiped_bones[slot] = null
+	#触发效果
 	var allowed_triggers:Array[Global.ItemCallBack] = [Global.ItemCallBack.ONUNLOAD,Global.ItemCallBack.EQUIPORUNLOAD]
 	var equip_context:= EffectContext.new()
 	equip_context.trigger_type = Global.ItemCallBack.ONUNLOAD
@@ -133,18 +157,55 @@ func _on_take_off_button_button_down() -> void:
 			if packed_effect.trigger == trigger:
 				packed_effect.effect(equip_context)
 	
-	current_selct_card = null
+	# inventory list中对应的卡片可见
+	card.show()
+		
+
+func _on_take_off_button_button_down() -> void:
+	if not (current_selct_card and current_selct_card.item):
+		push_error("aaaaaa")
+		return
+	# Move equipped item back to inventory list.
+	#var item := current_selct_card.item
+	#current_selct_card.item = null
+	#add_equipment_item_card(item)
+	
+	
+	#Global.equiped_bones[current_slot]= null
+	#item = item as BoneItem
+	#var allowed_triggers:Array[Global.ItemCallBack] = [Global.ItemCallBack.ONUNLOAD,Global.ItemCallBack.EQUIPORUNLOAD]
+	#var equip_context:= EffectContext.new()
+	#equip_context.trigger_type = Global.ItemCallBack.ONUNLOAD
+	## Roll back effects that should happen on unload.
+	#for packed_effect:PackedItemEffect in item.effects:
+		#for trigger in allowed_triggers:
+			#if packed_effect.trigger == trigger:
+				#packed_effect.effect(equip_context)
+	
+	#current_selct_card = null
+	#var card := slot_card[current_slot]
+	#if not card:
+		#push_error("equip card not found.")
+		#return
+	#card.show()
+	equip_take_off(current_slot)
+	
 	current_slot = -1
 	take_off_button.disabled = true
 
 
-func add_equipment_item_card(item:ItemBase):
+func add_equipment_item_card(item:ItemBase)-> ItemCard:
 	# Shared helper used by initial fill and unload operation.
 	var item_card := item_card_scene.instantiate() as ItemCard
 	item_card.item = item
 	equipments.add_child(item_card)
 	item_card.on_item_card_selected.connect(_on_item_card_selected)
+	return item_card
 
 
 func _on_next_wave_button_down() -> void:
 	on_equip_panel_next_wave.emit()
+
+
+func _on_inventory_pressed() -> void:
+	shop_pressed.emit()
