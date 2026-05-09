3d1e334d4c8b0f6832fe5af4a877b96cee593707
gwhyang <139050383+gwhyang@users.noreply.github.com>
2026-05-10 02:26:57 +0800

    1

--- assets/scenes/effects/enemy_spawn_effect.gd
+++
@@ -1,4 +0,0 @@
-extends Sprite2D
-class_name EnemySpawnEffect
-
-@onready var anim_player: AnimationPlayer = $AnimationPlayer
--- assets/scenes/ui/coins_bag/coins_bag.gd
+++
@@ -1,8 +0,0 @@
-extends HBoxContainer
-class_name CoinsBag
-
-@onready var coins: Label = $Coins
-
-func _process(delta: float) -> void:
-	coins.text = str(Global.coins)
-	
